433cc6990ad400cc4970eb335e90ecf06f125716
Samuelson Brito <samuelsonma@gmail.com>
2019-10-22 09:32:57 -0400

    divisão de pastas de front e back

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <div>
+
+    <Login/>
+
+
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Login from './components/Login'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Login
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ front/src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Acesso</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Login" name="login" type="text"></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
